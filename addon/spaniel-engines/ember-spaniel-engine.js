d57e98b46fc2e68e8da285374599f6f346868646
asakusuma <asa.kusuma@gmail.com>
2016-11-18 12:06:26 -0800

    Use runloop aware engine for spaniel

---
+++ addon/spaniel-engines/ember-spaniel-engine.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+const rAF = (typeof window === 'object') && typeof window.requestAnimationFrame === 'function' ? window.requestAnimationFrame : () => {};
+
+export default {
+  reads: [],
+  work: [],
+  running: false,
+  scheduleRead(callback) {
+    this.reads.unshift(callback);
+    this.run();
+  },
+  scheduleWork(callback) {
+    this.work.unshift(callback);
+    this.run();
+  },
+  run() {
+    if (!this.running) {
+      this.running = true;
+      rAF(() => {
+        Ember.run.join(() => {
+          for (let i = 0, rlen = this.reads.length; i < rlen; i++) {
+            this.reads.pop()();
+          }
+          for (let i = 0, wlen = this.work.length; i < wlen; i++) {
+            this.work.pop()();
+          }
+          this.running = false;
+          if (this.work.length > 0 || this.reads.length > 0) {
+            this.run();
+          }
+        });
+      });
+    }
+  }
+};
