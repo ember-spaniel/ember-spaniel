83142c57f48eaf7b0517474337639de7b0ca0e48
asakusuma <asa.kusuma@gmail.com>
2018-08-27 21:25:40 -0700

    Fix babel and eslint

--- prepublish.js
+++ prepublish.js
@@ -2,9 +2,4 @@
 const rollup = require('ember-rollup/src/prebuild'); 
 const addonPath = __dirname; 
  
-rollup.preBuild(addonPath).then(() => { 
-  process.exit(0); 
-}).catch(error => { 
-  console.error(error); 
-  process.exit(1); 
-});   
+rollup.preBuild(addonPath);
