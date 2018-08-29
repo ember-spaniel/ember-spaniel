026816459de708e6d9042725105c217b199460f4
asakusuma <asa.kusuma@gmail.com>
2018-08-28 18:16:12 -0700

    Add eslint ignore for workaround

--- prepublish.js
+++ prepublish.js
@@ -3,8 +3,12 @@ const rollup = require('ember-rollup/src/prebuild');
 const addonPath = __dirname; 
  
 rollup.preBuild(addonPath).then(() => {
+  // Work around https://github.com/asakusuma/ember-rollup/issues/44
+  // eslint-disable-next-line no-process-exit
   process.exit(0);
 }).catch((err) => {
   console.error(err);
+  // Work around https://github.com/asakusuma/ember-rollup/issues/44
+  // eslint-disable-next-line no-process-exit
   process.exit(1);
 });
