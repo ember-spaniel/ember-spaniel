a8cb32a801163cb92cfe2ec23f6925a5dfa5b518
Asa Kusuma <asa.kusuma@gmail.com>
2018-08-28 18:30:40 -0700

    Build upgrades (#29)
    * Move to yarn and upgrade build dependencies
    * Prebuild spaniel (had to work around https://github.com/asakusuma/ember-rollup/issues/44)
    * Drop support for node 4 and 6
    * Release 0.5.0
    * Add nuke script

---
+++ prepublish.js
@@ -0,0 +1,14 @@
+'use strict'; 
+const rollup = require('ember-rollup/src/prebuild'); 
+const addonPath = __dirname; 
+ 
+rollup.preBuild(addonPath).then(() => {
+  // Work around https://github.com/asakusuma/ember-rollup/issues/44
+  // eslint-disable-next-line no-process-exit
+  process.exit(0);
+}).catch((err) => {
+  console.error(err);
+  // Work around https://github.com/asakusuma/ember-rollup/issues/44
+  // eslint-disable-next-line no-process-exit
+  process.exit(1);
+});
